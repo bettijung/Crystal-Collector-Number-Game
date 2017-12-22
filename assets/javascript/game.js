7fb8984494627b0ae5046c7a52c9d764627ee68f
Bettina Junghahn <bettinajunghahn@Bettinas-MacBook-Pro.local>
2017-12-21 18:16:32 -0600

    push more code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,10 @@
 	var targetNumber = Math.floor(Math.random() * 101 + 1) + 19;
 	$("#number-to-guess").text(targetNumber);
 
-// Create on-click event that responds to button clicks of crystal images.
+// Create crystals and their counting values.
 	var counter = 0;
+	var wins = 0;
+	var losses = 0;
 
 	var crystalOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -17,30 +19,39 @@
 		imageCrystal.addClass("crystal-image");
 		imageCrystal.attr("src", "assets/images/sparkling-diamond-bling-animated-gif-21.gif");
 		
-		imageCrystal.attr("data-crystal-value", crystalOptions[i])
+		imageCrystal.attr("data-crystal-value", crystalOptions[i]);
 
 		$("#crystals").append(imageCrystal);
 	}
-
+// Make crystals working buttons that add thair values to the counter when clicked.
 	$(".crystal-image").on("click", function() {
 		var crystalValue = ($(this).attr("data-crystal-value"));
 		crystalValue = parseInt(crystalValue);
 
 		counter += crystalValue;
-		
-		// alert("New score: " + counter);
 		$("#user-total-score").text(counter);
 	
-	// "Check" if counter matches targetNumber.
+	// Mark wins and losses.
 		if(counter === targetNumber) {
-			alert("You Win!");
+			wins ++;
+			$("#win-score").text(wins);
+			resetGame();
 		}
 		else if(counter >= targetNumber) {
-			alert("You Lose!");
+			losses ++;
+			$("#lose-score").text(losses);
+			resetGame();
 		}
 
 	});
 
+// Reset game funciton.
+	function resetGame() {
+				counter = 0;
+				imageCrystal.attr("data-crystal-value", crystalOptions[i])
+
+		$("#crystals").append(imageCrystal);
+			}
 
 
 
